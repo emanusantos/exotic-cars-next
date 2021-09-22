63b39f045000d2aac1e04e8b5d61448039fc28c8
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-09-22 19:23:51 -0300

    feat: finished the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,21 +6,38 @@ import path from 'path';
 import fs from 'fs/promises';
 import { GetStaticProps } from 'next'
 
-const Home: NextPage = (props: any) => {
-  const { cars } = props;
+type CarResponse = {
+  id: number;
+  brand: string;
+  model: string;
+  price: number;
+  fileName: string;
+}
+
+type Props = {
+  cars: CarResponse[]
+}
+
+const Home: NextPage<Props> = ({ cars }) => {
   return (
     <>
     <Header />
     <S.Container>
-      {cars.map((car: any) => (
-        <Car key={car.id} brand={car.brand} model={car.model} price={car.price} img={car.fileName} />
+      {cars.map((car: CarResponse) => (
+        <Car 
+          key={car.id} 
+          brand={car.brand} 
+          model={car.model} 
+          price={car.price} 
+          img={car.fileName} 
+        />
       ))}
     </S.Container>
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async () => {
   const filePath = path.join(process.cwd(), 'src', 'data', 'cars.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData.toString());
