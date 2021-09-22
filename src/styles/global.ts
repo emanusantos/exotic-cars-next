af965e5c05a20573c81698e9fcc3b593343d6b84
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-09-22 16:41:53 -0300

    first commit: organized folders & added styled components dependencies, types and config

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    
+    body {
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+`;
+
+export default GlobalStyles;
